a1b04a7616721c5c4018610d2bdf0696ee6b2284
Jagdamba Tripathi <jagdtri2003@gmail.com>
2022-10-23 22:23:06 +0530

    modified:   Important/SolveNQueen.java

--- Important/SolveNQueen.java
+++ Important/SolveNQueen.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class SolveQueen {
+public class SolveNQueen {
     public static boolean isSafe(int row, int col, char[][] board) {
         //horizontal
         for(int j=0; j<board.length; j++) {
